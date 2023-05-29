a56bcb66a712bf5afc304aaa64aa7906d969ee58
epicCreator <134795677+epicCreator@users.noreply.github.com>
2023-05-29 10:14:01 -0400

    Update MyScript.js

--- MyScript.js
+++ MyScript.js
@@ -1,13 +1,14 @@
 document.getElementById("search-button").addEventListener("click", function() {
-  var locationInput = document.getElementById("location-input").value.toLowerCase();
-  var jobListings = document.querySelectorAll("ul li");
-  
-  for (var i = 0; i < jobListings.length; i++) {
-    var jobLocation = jobListings[i].querySelector(".location").textContent.toLowerCase();
-    if (jobLocation.indexOf(locationInput) > -1) {
-      jobListings[i].style.display = "";
-    } else {
-      jobListings[i].style.display = "none";
+    var locationInput = document.getElementById("location-input").value.toLowerCase();
+    var jobListings = document.querySelectorAll("ul li");
+    
+    for (var i = 0; i < jobListings.length; i++) {
+      var jobLocation = jobListings[i].querySelector(".location").textContent.toLowerCase();
+      if (jobLocation.indexOf(locationInput) > -1) {
+        jobListings[i].style.display = "";
+      } else {
+        jobListings[i].style.display = "none";
+      }
     }
-  }
-});
+  });
+  
